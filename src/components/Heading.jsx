6434230bfb453ea9876426973b598ac800a1adc8
rachelharu <rachel.haru9@gmail.com>
2022-03-07 12:55:09 -0800

    create heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Heading(){
+
+    return (
+
+<header className="header">
+
+      <h1 className="h1">Keeper</h1>
+
+</header>
+
+    );
+}
+
+export default Heading;
